bede23cb2af715d26a3d008b650d76a1dc889d47
SirLez <76112117+SirLez@users.noreply.github.com>
2021-09-19 16:43:47 +0300

    Update __init__.py

--- samino/__init__.py
+++ samino/__init__.py
@@ -7,7 +7,7 @@ from requests import get
 
 # By SirLez
 try:
-    version = '1.5.3'
+    version = '1.5.4'
     newest = get("https://pypi.python.org/pypi/SAmino/json").json()["info"]["version"]
     if version != newest: print(f"\033[1;33mSAmino New Version!: {newest} (Your Using {version})\033[1;0m")
 except: pass
