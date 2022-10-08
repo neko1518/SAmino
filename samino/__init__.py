4edb4a1958c39c05f2b39e3addd89d0e66374db5
SirLez <76112117+SirLez@users.noreply.github.com>
2022-10-09 00:15:11 +0300

    v2.3.5

--- samino/__init__.py
+++ samino/__init__.py
@@ -6,7 +6,7 @@ from .SAsync import *
 from requests import get
 
 try:
-    version = "2.3.4"
+    version = "2.3.5"
     newest = get("https://pypi.python.org/pypi/SAmino/json").json()["info"]["version"]
     if version != newest:
         print(f"\033[1;33mSAmino New Version!: {newest} (Your Using {version})\033[1;0m")
